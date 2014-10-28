76b0c2f35e5827e22a29756abdcf21bf47f272c7
Tomas Marcote <tomas.marcote@gmail.com>
2014-10-28 14:52:20 -0300

    agregadas push notifications. Agregado cierre de la splash screen en el evente ondeviceready

--- platforms/android/ant-build/JSConf.AR-debug-unaligned.apk.d
+++ platforms/android/ant-build/JSConf.AR-debug-unaligned.apk.d
@@ -2,3 +2,5 @@
 /home/tomas/test/platforms/android/ant-build/JSConf.AR.ap_ \
 /home/tomas/test/platforms/android/ant-build/classes.dex \
 /home/tomas/test/platforms/android/CordovaLib/ant-build/classes.jar \
+/home/tomas/test/platforms/android/libs/gcm.jar \
+/home/tomas/test/platforms/android/libs/android-support-v13.jar \
--- www/js/app.js
+++ www/js/app.js
@@ -6,12 +6,14 @@ var $$ = Dom7;
 var myApp = new Framework7({
   //modal para el chat plugin
   modalTitle: 'Welcome',
-  animateNavBackIcon: true //swipePanel: 'left'
+  animateNavBackIcon: true, //swipePanel: 'left'
+  fastClicks: true
 });
 
 //Esta variable es usada tanto por el plugin de login como por el plugin de chat
 var username, avatar = false,
-  social = "";
+  social = "",
+  pushNotification;
 
 initLogin();
 initViews();
@@ -563,8 +565,38 @@ function initLogout() {
 //CONNECTION STATUS (necesita el plugin org.apache.cordova.network-information)
 
 function onDeviceReady() {
+
   document.addEventListener("online", onOnline, false);
   document.addEventListener("offline", onOffline, false);
+
+  //oculto la splashscreen 
+  navigator.splashscreen.hide();
+
+  // $("#app-status-ul").append('<li>deviceready event received</li>');
+  //preparo para recibir push notifications
+  try {
+    pushNotification = window.plugins.pushNotification;
+    // $("#app-status-ul").append('<li>registering ' + device.platform + '</li>');
+    if (device.platform == 'android' || device.platform == 'Android' ||
+      device.platform == 'amazon-fireos') {
+      pushNotification.register(successHandler, errorHandler, {
+        "senderID": "153292884918", //id de mi proyecto en Google developer console
+        "ecb": "onNotification" //funcion a ejecutar si estoy en android
+      }); // required!
+    } else {
+      pushNotification.register(tokenHandler, errorHandler, {
+        "badge": "true",
+        "sound": "true",
+        "alert": "true",
+        "ecb": "onNotificationAPN"
+      }); // required!
+    }
+  } catch (err) {
+    txt = "There was an error on this page.\n\n";
+    txt += "Error description: " + err.message + "\n\n";
+    alert(txt);
+  }
+
 }
 
 // Handle the online event
@@ -577,4 +609,119 @@ function onOnline() {
 //
 function onOffline() {
   $$('.needs-conn').addClass('disabled');
+}
+
+
+
+//FUNCIONES DE PUSH NOTIFICATION
+
+
+// handle APNS notifications for iOS
+function onNotificationAPN(e) {
+  if (e.alert) {
+    // $("#app-status-ul").append('<li>push-notification: ' + e.alert + '</li>');
+    // showing an alert also requires the org.apache.cordova.dialogs plugin
+    navigator.notification.alert(e.alert);
+  }
+
+  if (e.sound) {
+    // playing a sound also requires the org.apache.cordova.media plugin
+    var snd = new Media(e.sound);
+    snd.play();
+  }
+
+  if (e.badge) {
+    pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
+  }
+}
+
+// handle GCM notifications for Android
+function onNotification(e) {
+
+  var registerDB = new Firebase("https://shovelChat.firebaseio.com/register");
+  // $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+
+  switch (e.event) {
+    case 'registered':
+      if (e.regid.length > 0) {
+
+
+        //checkeo si esta instancia de la app ya esta dada de alta para las push notif.
+        registerDB.once('value', function(snapshot) {
+          if (!snapshot.hasChild(e.regid)) {
+            registerDB.child(e.regid).set({
+              name: e.regid
+            });
+          }
+        });
+
+        // $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+        // Your GCM push server needs to know the regID before it can push to this device
+        // here is where you might want to send it the regID for later use.
+        // console.log("regID = " + e.regid);
+      }
+      break;
+
+    case 'message':
+      // if this flag is set, this notification happened while we were in the foreground.
+      // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+      // alert(e.data);
+      if (!e.foreground) {
+        // $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+
+        // on Android soundname is outside the payload. 
+        // On Amazon FireOS all custom attributes are contained within payload
+        var soundfile = e.payload.sound;
+
+        var path = window.location.pathname;
+        //-10 porque remueve index.html
+        path = path.substr(path, path.length - 10);
+        path = 'file://' + path;
+        // if the notification contains a soundname, play it.
+        // playing a sound also requires the org.apache.cordova.media plugin
+        var my_media = new Media(path + 'sounds/' + soundfile);
+        my_media.play();
+
+      }
+
+      myApp.addNotification({
+        title: e.payload.title,
+        message: e.payload.message
+      });
+      // } else { // otherwise we were launched because the user touched a notification in the notification tray.
+      //   if (e.coldstart)
+      //     $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+      //   else
+      //     $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+      // }
+
+      // $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+      // //android only
+      // $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+      // //amazon-fireos only
+      // $("#app-status-ul").append('<li>MESSAGE -> TIMESTAMP: ' + e.payload.timeStamp + '</li>');
+      break;
+
+    case 'error':
+      // $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+      break;
+
+    default:
+      // $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+      break;
+  }
+}
+
+function tokenHandler(result) {
+  // $("#app-status-ul").append('<li>token: ' + result + '</li>');
+  // Your iOS push server needs to know the token before it can push to this device
+  // here is where you might want to send it the token for later use.
+}
+
+function successHandler(result) {
+  // $("#app-status-ul").append('<li>success:' + result + '</li>');
+}
+
+function errorHandler(error) {
+  // $("#app-status-ul").append('<li>error:' + error + '</li>');
 }
