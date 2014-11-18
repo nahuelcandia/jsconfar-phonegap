a38a3528878439d0f518353b5ec6b08758bfd289
Tomas Marcote <tomas.marcote@gmail.com>
2014-11-18 17:49:47 -0300

    arreglado estilo de botones login, tiempos se actualizan en mensajes

--- www/js/app.js
+++ www/js/app.js
@@ -13,7 +13,7 @@ var myApp = new Framework7({
 //Esta variable es usada tanto por el plugin de login como por el plugin de chat
 var username, avatar = false,
   social = "",
-  pushNotification;
+  pushNotification, mensajesCargados = false;
 
 var passwords = [];
 var lat, lng, mapPH, markerPH;
@@ -22,6 +22,13 @@ initLogin();
 initViews();
 initLogout();
 
+$(document).ready(function() {
+  setInterval(function() {
+    $('.time').map(function(idx, element) {
+      $(element).text(moment($(element).data().time).fromNow());
+    })
+  }, 1000);
+});
 
 function initViews() {
   //vista latest tweets
@@ -77,7 +84,6 @@ function initMensajes() {
 
   //carga inicial
   mensajesDB.on("child_added", function(snapshot) {
-    console.log(snapshot.val());
     var mensaje = snapshot.val();
     var time = new Date(mensaje.date);
     if (mensaje.tipo == 1) {
@@ -85,8 +91,9 @@ function initMensajes() {
         '       <div class="content-block-inner blockMensajes">' +
         '         <p>' + mensaje.mensaje + '</p>' +
         '           <div class="content-block-title timeMensajes">' +
-        '             <i class="icon iconTimey"></i>' + moment(time).fromNow(); +
-        '           </div>' +
+        '             <i class="icon iconTimey"></i>' + '<span class="time" data-time="' + moment(time) +
+        '">' + moment(time).fromNow(); + '</span>'
+      '           </div>' +
         '       </div>';
     } else if (mensaje.tipo == 2) {
       var div =
@@ -97,10 +104,13 @@ function initMensajes() {
         '           </div>' +
         '       </div>';
     }
-
     $$('.listaMensajes').prepend(div);
 
-
+    //al cargar uno remuevo el preloader (spiner)
+    if (mensajesCargados == false) {
+      $$('.preloaderContainer').html("");
+      mensajesCargados = true;
+    }
 
   });
 
@@ -580,7 +590,7 @@ function initSpeakers(view) {
       '                </div>' +
       '                <div class="item-inner">' +
       '                  <div class="item-title-row">' +
-      '                    <a class="external" href="' + speakers[index].GithubUrl + '"><div class="item-title">' + speakers[index].Github + '</div></a>' +
+      '                    <div class="item-title">' + speakers[index].Github + '</div></a>' +
       '                  </div>' +
       '                </div>' +
       '              </div>' +
@@ -592,7 +602,7 @@ function initSpeakers(view) {
       '                </div>' +
       '                <div class="item-inner">' +
       '                  <div class="item-title-row">' +
-      '                    <a class="external" href="' + speakers[index].TwitterUrl + '"><div class="item-title">' + speakers[index].Twitter + '</div></a>' +
+      '                    <div class="item-title">' + speakers[index].Twitter + '</div></a>' +
       '                  </div>' +
       '                </div>' +
       '              </div>' +
@@ -839,10 +849,6 @@ function initLogout() {
 
 function onDeviceReady() {
 
-
-
-
-
   document.addEventListener("online", onOnline, false);
   document.addEventListener("offline", onOffline, false);
 
@@ -855,34 +861,34 @@ function onDeviceReady() {
 
   // $("#app-status-ul").append('<li>deviceready event received</li>');
   //preparo para recibir push notifications
-  try {
-    pushNotification = window.plugins.pushNotification;
-    // $("#app-status-ul").append('<li>registering ' + device.platform + '</li>');
-    if (device.platform == 'android' || device.platform == 'Android' ||
-      device.platform == 'amazon-fireos') {
+  // try {
+  //   pushNotification = window.plugins.pushNotification;
+  //   // $("#app-status-ul").append('<li>registering ' + device.platform + '</li>');
+  //   if (device.platform == 'android' || device.platform == 'Android' ||
+  //     device.platform == 'amazon-fireos') {
 
-      pushNotification.register(successHandler, errorHandler, {
-        "senderID": "153292884918", //id de mi proyecto en Google developer console
-        "ecb": "onNotification" //funcion a ejecutar si estoy en android
-      }); // required!
-    } else {
-      pushNotification.register(tokenHandler, errorHandler, {
-        "badge": "true",
-        "sound": "true",
-        "alert": "true",
-        "ecb": "onNotificationAPN"
-      }); // required!
-    }
-  } catch (err) {
-    txt = "There was an error on this page.\n\n";
-    txt += "Error description: " + err.message + "\n\n";
-    alert(txt);
-  }
+  //     pushNotification.register(successHandler, errorHandler, {
+  //       "senderID": "153292884918", //id de mi proyecto en Google developer console
+  //       "ecb": "onNotification" //funcion a ejecutar si estoy en android
+  //     }); // required!
+  //   } else {
+  //     pushNotification.register(tokenHandler, errorHandler, {
+  //       "badge": "true",
+  //       "sound": "true",
+  //       "alert": "true",
+  //       "ecb": "onNotificationAPN"
+  //     }); // required!
+  //   }
+  // } catch (err) {
+  //   txt = "There was an error on this page.\n\n";
+  //   txt += "Error description: " + err.message + "\n\n";
+  //   alert(txt);
+  // }
 
 }
 
 // Handle the online event
-//
+
 function onOnline() {
   $$('.needs-conn').removeClass('disabled');
 }
@@ -901,105 +907,105 @@ function weNeedToGoBack() {
 
 
 // handle APNS notifications for iOS
-function onNotificationAPN(e) {
-  if (e.alert) {
-    // $("#app-status-ul").append('<li>push-notification: ' + e.alert + '</li>');
-    // showing an alert also requires the org.apache.cordova.dialogs plugin
-    navigator.notification.alert(e.alert);
-  }
+// function onNotificationAPN(e) {
+//   if (e.alert) {
+//     // $("#app-status-ul").append('<li>push-notification: ' + e.alert + '</li>');
+//     // showing an alert also requires the org.apache.cordova.dialogs plugin
+//     navigator.notification.alert(e.alert);
+//   }
 
-  if (e.sound) {
-    // playing a sound also requires the org.apache.cordova.media plugin
-    var snd = new Media(e.sound);
-    snd.play();
-  }
+//   if (e.sound) {
+//     // playing a sound also requires the org.apache.cordova.media plugin
+//     var snd = new Media(e.sound);
+//     snd.play();
+//   }
 
-  if (e.badge) {
-    pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
-  }
-}
+//   if (e.badge) {
+//     pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
+//   }
+// }
 
-// handle GCM notifications for Android
-function onNotification(e) {
+// // handle GCM notifications for Android
+// function onNotification(e) {
 
-  var registerDB = new Firebase("https://shovelChat.firebaseio.com/register");
-  // $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+//   var registerDB = new Firebase("https://shovelChat.firebaseio.com/register");
+//   // $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
 
-  switch (e.event) {
-    case 'registered':
-      if (e.regid.length > 0) {
+//   switch (e.event) {
+//     case 'registered':
+//       if (e.regid.length > 0) {
 
 
-        //checkeo si esta instancia de la app ya esta dada de alta para las push notif.
-        registerDB.once('value', function(snapshot) {
-          if (!snapshot.hasChild(device.uuid)) {
-            registerDB.child(device.uuid).set({
-              gcmId: e.regid
-            });
-          }
-        });
+//         //checkeo si esta instancia de la app ya esta dada de alta para las push notif.
+//         registerDB.once('value', function(snapshot) {
+//           if (!snapshot.hasChild(device.uuid)) {
+//             registerDB.child(device.uuid).set({
+//               gcmId: e.regid
+//             });
+//           }
+//         });
 
-        // $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
-        // Your GCM push server needs to know the regID before it can push to this device
-        // here is where you might want to send it the regID for later use.
-        // console.log("regID = " + e.regid);
-      }
-      break;
+//         // $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+//         // Your GCM push server needs to know the regID before it can push to this device
+//         // here is where you might want to send it the regID for later use.
+//         // console.log("regID = " + e.regid);
+//       }
+//       break;
 
-    case 'message':
-      // if this flag is set, this notification happened while we were in the foreground.
-      // you might want to play a sound to get the user's attention, throw up a dialog, etc.
-      // alert(e.data);
-      // if (e.foreground) {
-      // $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+//     case 'message':
+//       // if this flag is set, this notification happened while we were in the foreground.
+//       // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+//       // alert(e.data);
+//       // if (e.foreground) {
+//       // $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
 
-      // on Android soundname is outside the payload. 
-      // On Amazon FireOS all custom attributes are contained within payload
-      var soundfile = e.payload.sound;
+//       // on Android soundname is outside the payload. 
+//       // On Amazon FireOS all custom attributes are contained within payload
+//       var soundfile = e.payload.sound;
 
-      var path = window.location.pathname;
-      //-10 porque remueve index.html
-      path = path.substr(path, path.length - 10);
-      path = 'file://' + path;
-      // if the notification contains a soundname, play it.
-      // playing a sound also requires the org.apache.cordova.media plugin
-      var my_media = new Media(path + 'sounds/' + soundfile);
-      my_media.play();
+//       var path = window.location.pathname;
+//       //-10 porque remueve index.html
+//       path = path.substr(path, path.length - 10);
+//       path = 'file://' + path;
+//       // if the notification contains a soundname, play it.
+//       // playing a sound also requires the org.apache.cordova.media plugin
+//       var my_media = new Media(path + 'sounds/' + soundfile);
+//       my_media.play();
 
-      // }
-      // else { // otherwise we were launched because the user touched a notification in the notification tray.
-      //   if (e.coldstart)
-      //     $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
-      //   else
-      //     $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
-      // }
+//       // }
+//       // else { // otherwise we were launched because the user touched a notification in the notification tray.
+//       //   if (e.coldstart)
+//       //     $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+//       //   else
+//       //     $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+//       // }
 
-      myApp.addNotification({
-        title: e.payload.title,
-        message: e.payload.message
-      });
-      break;
+//       myApp.addNotification({
+//         title: e.payload.title,
+//         message: e.payload.message
+//       });
+//       break;
 
-    case 'error':
-      // $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
-      break;
+//     case 'error':
+//       // $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+//       break;
 
-    default:
-      // $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
-      break;
-  }
-}
+//     default:
+//       // $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+//       break;
+//   }
+// }
 
-function tokenHandler(result) {
-  // $("#app-status-ul").append('<li>token: ' + result + '</li>');
-  // Your iOS push server needs to know the token before it can push to this device
-  // here is where you might want to send it the token for later use.
-}
+// function tokenHandler(result) {
+//   // $("#app-status-ul").append('<li>token: ' + result + '</li>');
+//   // Your iOS push server needs to know the token before it can push to this device
+//   // here is where you might want to send it the token for later use.
+// }
 
-function successHandler(result) {
-  // $("#app-status-ul").append('<li>success:' + result + '</li>');
-}
+// function successHandler(result) {
+//   // $("#app-status-ul").append('<li>success:' + result + '</li>');
+// }
 
-function errorHandler(error) {
-  // $("#app-status-ul").append('<li>error:' + error + '</li>');
-}
+// function errorHandler(error) {
+//   // $("#app-status-ul").append('<li>error:' + error + '</li>');
+// }
